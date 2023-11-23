47ba7b2ec04c242234a5804aa3163a12b503fe23
VManonmanian <manosir1998@gmail.com>
2023-11-23 23:28:55 +0530

    DELLET EMTY LINE

--- src/main/java/com/bank/dao/BankDao.java
+++ src/main/java/com/bank/dao/BankDao.java
@@ -12,19 +12,16 @@ public class BankDao {
 	@Autowired
 	BankRepository bankRepo;
 	public String setBankDetail(Bank bank) {
-		// TODO Auto-generated method stub
 		bankRepo.save(bank);
 		return "Saved bank detail";
 	}
 
 	public String setAllBankDetail(List<Bank> banks) {
-		// TODO Auto-generated method stub
 		bankRepo.saveAll(banks);
 		return "Saved banks details";
 	}
 
 	public int getBalance(long acNo) {
-		// TODO Auto-generated method stub
 		return bankRepo.getBalance(acNo);
 	}
 
